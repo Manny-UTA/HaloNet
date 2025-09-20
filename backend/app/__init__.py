bae730cff83a01b89881a07486c4962ace98c70f
Harrison Stein <hstein@smu.edu>
2025-09-20 12:46:27 -0500

    API setup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,1 +1,9 @@
-# init file for backend app
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MOCK_SAFETY_API_KEY = os.getenv("MOCK_SAFETY_API_KEY")
+CHATGPT_API_KEY = os.getenv("CHATGPT_API_KEY")
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
